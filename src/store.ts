2ef919f63a3efb34173a530f0ad3ab85af5b3186
hornta <hornta@protonmail.com>
2023-04-13 02:15:14 +0200

    make the editor working

---
+++ src/store.ts
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { api } from "./api.js";
+
+export const store = configureStore({
+	reducer: { [api.reducerPath]: api.reducer },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
