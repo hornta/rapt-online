fd882856bd05ba95654857be391492c8f3c8bd65
hornta <hornta@protonmail.com>
2023-04-15 01:15:57 +0200

    fix build 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default async function Home() {
 	const levels = await prisma.level.findMany({
-		orderBy: { createdBy: "desc" },
+		orderBy: { createdAt: "desc" },
 	});
 
 	return (
